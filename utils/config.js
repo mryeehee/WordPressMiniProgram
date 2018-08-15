27263c4c098f132d3c4c2429659ab0754421efd2
mryeehee <linmuyuetkm@gmail.com>
2018-08-15 22:59:13 +0800

    重新修复了官方最新的navigateTo跳转方式

--- utils/config.js
+++ utils/config.js
@@ -11,9 +11,9 @@
 
 
 
-//配置域名,域名只修改此处。
+
 //如果wordpress没有安装在网站根目录请加上目录路径,例如："www.yeehee.cn/blog"
-var DOMAIN = "www.yeehee.cn";
+var DOMAIN = "www.yeehee.cn";//配置域名,域名只修改此处。
 var MINAPPTYPE="1";//小程序的类型，如果是企业小程序请填：0 ，如果是个人小程序请填：1
 var WEBSITENAME="叶赫先生"; //网站名称
 var ABOUTID = 2; //wordpress网站"页面"的id,注意这个"页面"是wordpress的"页面"，不是"文章"
